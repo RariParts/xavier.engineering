f90c7cd43ddf3fef050df6335e1c857a61b35648
xavier <xmd404@pm.me>
2022-01-08 13:33:29 -0500

    add polyglot description

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// jQuery function repsonsible for polyglot greeting on about page
+$(document).ready(function(){
+  rotation = [
+    "beach houses & friends",
+    "multihyphanite-wanderlust",
+    "rip curls & flip-flops",
+    "engineer-as-a-service"
+  ]
+
+  var i = 0;
+
+  setInterval(function(){
+    $('#options').fadeOut(1000, function(){
+      $(this).text(rotation[i++ % rotation.length]).fadeIn(1000);
+    });
+  }, 7000)
+});
