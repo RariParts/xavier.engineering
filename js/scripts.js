fe4a208d1fdcb153f4f230c06ce219d4cffe4b99
xavier <xmd404@pm.me>
2022-04-17 07:25:52 -0600

    refactor and add new links

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,24 @@ $(document).ready(function(){
     });
   }, 7000)
 });
+
+// showHide
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, menu__item;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="menu__item" and remove the class "active"
+  menu__item = document.getElementsByClassName("menu__item");
+  for (i = 0; i < menu__item.length; i++) {
+    menu__item[i].className = menu__item[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+}
